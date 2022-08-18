631162b8e9a6862fb8254cf1ff6db8e75959ceff
pennyluc <pennyluc321@gmail.com>
2022-08-18 17:46:20 +0100

    navbar component set up and styled

--- portfolio./src/App.js
+++ portfolio./src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div>
-    Hello
+    <Navbar/>
+    
 
     </div>
     
---
+++ portfolio./src/components/Navbar.js
@@ -0,0 +1,41 @@
+import './Navbar.css'
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const Navbar = () => {
+const [click, setClick] = useState(false)
+
+const handleClick = () => setClick(!click)
+
+    return ( 
+        <div className='header'>
+        <nav className='navbar'>
+            <a href ="/" className='logo'>
+                <img alt="logo"></img>
+            </a>
+        <div className='hamburger' onClick={handleClick}>
+        {click ? (<FaTimes size={30} style={{color:'white'}}/>) : (<FaBars size={30} style={{color:'white'}}/>)}
+        
+
+        </div>
+        <ul className={click ? "nav-menu active": "nav-menu"}>
+            <li className='nav-item'>
+                <a href="/">Home</a>
+            </li>
+            <li className='nav-item'>
+                <a href="/">About</a>
+            </li>
+            <li className='nav-item'>
+                <a href="/">Test</a>
+            </li>
+            <li className='nav-item'>
+                <a href="/">Demo</a>
+            </li>
+        </ul>
+</nav>
+        </div>
+
+     );
+}
+ 
+export default Navbar;
